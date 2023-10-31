cdeb3829bb8dc37492883f9b54051fa05dfeaa1c
Salvador Alvarado <salvador-ivan24@hotmail.com>
2023-10-30 21:24:42 -0500

    save app 2

--- app/main.py
+++ app/main.py
@@ -5,37 +5,24 @@ import read_csv
 
 
 def run():
-  data = read_csv.read_csv("./app/data.csv")
+  data = read_csv.read_csv("data.csv")
   data = list(filter(lambda item: item["Continent"] == "South America", data))
 
   country = list(map(lambda x: x["Country/Territory"], data))
   percentaje = list(map(lambda x: x["World Population Percentage"], data))
 
-  #charts.generate_bar_chart(country, percentaje)
   charts.generate_pie_chart(country, percentaje)
 
-  
-  
-  #labels = ["World Population Percentage"]
-  #tr = list(map(lambda ee: ee +1, percentaje))
-  #print(tr)
-  
-  #values = int(percentaje)
-  #charts.generate_bar_chart(labels, values)
 
-
-
-
-  '''
   country = input("Ingresa tu pais => ")
   result = utils.population_by_country(data, country)
 
   if len(result) > 0:
     country = result[0]
     labels, values = utils.get_population(country)
-    charts.generate_bar_chart(labels, values)
+    charts.generate_bar_chart(country["Country/Territory"], labels, values)
 
-  '''
+
 
 
 
